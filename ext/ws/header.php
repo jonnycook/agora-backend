4a8c0e12acd5560d98f7cac228fdb9c8ac67a926
Jonny <qubsoft@gmail.com>
2014-06-14 10:45:06 +0700

    .

--- ext/ws/header.php
+++ ext/ws/header.php
@@ -19,3 +19,112 @@ function getUserId($clientId) {
 	}
 	return $row[0];
 }
+
+function dbErrors() {
+	// Error handler 1
+	function myErrorHandler($errno, $errstr, $errfile, $errline, $errcontext)
+	{
+	    if ($errno == E_NOTICE) {
+	        // This error code is not included in error_reporting
+	        return;
+	    }
+
+		$values = array_merge(getRequestParams(), array(
+			'error_number' => $errno,
+			'error_string' => $errstr,
+			'error_file' => $errfile,
+			'error_line' => $errline,
+			'error_context' => serialize($errcontext),
+			'function_stack' => serialize(xdebug_get_function_stack()),
+		));
+
+		insert($values, 'update_errors');
+		echo 'error';
+
+	    return true;
+	}
+
+	// set to the user defined error handler
+	$old_error_handler = set_error_handler("myErrorHandler");
+
+	function exceptionHandler($e) {
+		$values = array_merge(getRequestParams(), array(
+			'error_string' => $e->getMessage(),
+			'error_file' => $e->getFile(),
+			'error_line' => $e->getLine(),
+			'function_stack' => serialize(xdebug_get_function_stack()),
+		));
+
+		insert($values, 'update_errors');
+		echo 'error';
+
+	    return true;
+	}
+
+	set_exception_handler('exceptionHandler');
+
+	// Error handler 2
+	error_reporting(E_ALL);
+	ini_set('display_errors', 0);
+
+	function shutdown() {
+	    $isError = false;
+	    if ($error = error_get_last()){
+	        switch($error['type']){
+	            case E_ERROR:
+	            case E_CORE_ERROR:
+	            case E_COMPILE_ERROR:
+	            case E_USER_ERROR:
+	                $isError = true;
+	                break;
+	        }
+	    }
+
+	    if ($isError) {
+			$values = array_merge(getRequestParams(), array(
+				'error_number' => $error['type'],
+				'error_string' => $error['message'],
+				'error_file' => $error['file'],
+				'error_line' => $error['line'],
+				'function_stack' => serialize(xdebug_get_function_stack()),
+			));
+
+			insert($values, 'update_errors');
+
+			echo '"error"';
+	    }
+	}
+	// register_shutdown_function('shutdown');
+}
+
+
+function getRequestParams() {
+	global $db, $passedId, $lastTime, $params;
+
+	$values = array(
+		'user_id' => $db->userId,
+		'ip' => remoteAddr(),
+		'request_clientId' => $params['clientId'],
+		'request_extVersion' => $params['extVersion'],
+		'request_apiVersion' => $params['apiVersion'],
+		'request_instanceId' => $params['instanceId'],
+		'request_userId' => $passedId,
+		'request_lastTime' => $lastTime ? $lastTime : '0000-00-00 00:00:00',
+		'request_changes' => $params['changes'],
+	);
+
+	return $values;
+}
+
+function insert($values, $table) {
+	global $mysqli;
+	foreach ($values as $field => $value) {
+		$setters[] = "`$field` = '" . mysqli_real_escape_string($mysqli, $value) . '\'';
+	}
+
+	$query = "INSERT INTO `$table` SET " . implode(',', $setters);
+	mysqli_query($mysqli, $query) or die("$query: " . mysqli_error($mysqli));
+}
+
+
+dbErrors();
