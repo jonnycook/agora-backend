4075093472487b47b53708ff3243133b075c537c
Jonny <qubsoft@gmail.com>
2014-07-04 20:20:25 +0700

    .

--- migrate.php
+++ migrate.php
@@ -22,70 +22,90 @@ header('Content-Type: text/plain');
 // 	}
 // }
 
+// $tables = array(
+// 	'm_list_elements',
+// 	'm_belt_elements',
+// 	'm_session_elements',
+// 	'm_bundle_elements',
+// 	'm_feelings',
+// 	'm_data',
+// 	'm_root_elements',
+// );
+
+// $result = mysqli_query($mysqli, 'SELECT * FROM m_products_old');
+// while ($row = mysqli_fetch_assoc($result)) {
+// 	$products[$row['site_id']][$row['sid']][] = $row;
+// }
+
+// function value($value) {
+// 	if ($value == null) {
+// 		return 'NULL';
+// 	}
+// 	else {
+// 		global $mysqli;
+// 		return '"' . mysqli_real_escape_string($mysqli, $value) . '"';
+// 	}
+// }
+
+// set_time_limit(0);
+
+// mysqli_query($mysqli, 'TRUNCATE TABLE m_products');
+
+// foreach ($products as $siteId => $siteProducts) {
+// 	foreach ($siteProducts as $sid => $rows) {
+// 		$row = $rows[0];
+// 		mysqli_query($mysqli, "INSERT INTO m_products SET " .
+// 			'`type` = ' . value($row['type']) . ',' .
+// 			'`sid` = ' . value($row['sid']) . ',' .
+// 			'`site_id` = ' . value($row['site_id']) . ',' .
+// 			'`title` = ' . value($row['title']) . ',' .
+// 			'`image_url` = ' . value($row['image_url']) . ',' .
+// 			'`price` = ' . value($row['price']) . ',' .
+// 			'`retrieval_id` = ' . value($row['retrieval_id']) . ',' .
+// 			'`currency` = ' . value($row['currency']) . ',' .
+// 			'`url` = ' . value($row['url']) . ',' .
+// 			'`offer` = ' . value($row['offer']) . ',' .
+// 			'`rating` = ' . value($row['rating']) . ',' .
+// 			'`rating_count` = ' . value($row['rating_count']) . ',' .
+// 			'`created_at` = ' . value($row['created_at']) . ',' .
+// 			'`last_scraped_at` = ' . value($row['last_scraped_at']) . ',' .
+// 			'`status` = ' . value($row['status'])) or die(mysqli_error($mysqli));
+
+// 		$id = mysqli_insert_id($mysqli);
+// 		$newId[$siteId][$sid] = $id;
+// 	}
+// }
+
+// var_dump($newId);
+
+// foreach ($products as $siteId => $siteProducts) {
+// 	foreach ($siteProducts as $sid => $rows) {
+// 		$ids = array_map(function($row) { return $row['id']; }, $rows);
+// 		$id = $newId[$siteId][$sid];
+// 		foreach ($tables as $table) {
+// 			$sql = "UPDATE $table SET updated=1, element_id = $id WHERE !updated && element_type = 'Product' && element_id IN (" . implode(', ', $ids) . ')';
+// 			// echo "$sql\n";
+// 			mysqli_query($mysqli, $sql) or die(mysqli_error($msyqli));
+// 		}
+// 	}
+// }
+
 $tables = array(
-	'm_list_elements',
+	'm_belts',
 	'm_belt_elements',
-	'm_session_elements',
+	'm_bundles',
 	'm_bundle_elements',
-	'm_feelings',
 	'm_data',
+	'm_decisions',
+	'm_descriptors',
+	'm_lists',
+	'm_list_elements',
+	'm_object_references',
 	'm_root_elements',
+	'm_sessions',
+	'm_session_elements',
 );
 
-$result = mysqli_query($mysqli, 'SELECT * FROM m_products_old');
-while ($row = mysqli_fetch_assoc($result)) {
-	$products[$row['site_id']][$row['sid']][] = $row;
-}
-
-function value($value) {
-	if ($value == null) {
-		return 'NULL';
-	}
-	else {
-		global $mysqli;
-		return '"' . mysqli_real_escape_string($mysqli, $value) . '"';
-	}
-}
-
-set_time_limit(0);
-
-mysqli_query($mysqli, 'TRUNCATE TABLE m_products');
-
-foreach ($products as $siteId => $siteProducts) {
-	foreach ($siteProducts as $sid => $rows) {
-		$row = $rows[0];
-		mysqli_query($mysqli, "INSERT INTO m_products SET " .
-			'`type` = ' . value($row['type']) . ',' .
-			'`sid` = ' . value($row['sid']) . ',' .
-			'`site_id` = ' . value($row['site_id']) . ',' .
-			'`title` = ' . value($row['title']) . ',' .
-			'`image_url` = ' . value($row['image_url']) . ',' .
-			'`price` = ' . value($row['price']) . ',' .
-			'`retrieval_id` = ' . value($row['retrieval_id']) . ',' .
-			'`currency` = ' . value($row['currency']) . ',' .
-			'`url` = ' . value($row['url']) . ',' .
-			'`offer` = ' . value($row['offer']) . ',' .
-			'`rating` = ' . value($row['rating']) . ',' .
-			'`rating_count` = ' . value($row['rating_count']) . ',' .
-			'`created_at` = ' . value($row['created_at']) . ',' .
-			'`last_scraped_at` = ' . value($row['last_scraped_at']) . ',' .
-			'`status` = ' . value($row['status'])) or die(mysqli_error($mysqli));
-
-		$id = mysqli_insert_id($mysqli);
-		$newId[$siteId][$sid] = $id;
-	}
-}
-
-var_dump($newId);
-
-foreach ($products as $siteId => $siteProducts) {
-	foreach ($siteProducts as $sid => $rows) {
-		$ids = array_map(function($row) { return $row['id']; }, $rows);
-		$id = $newId[$siteId][$sid];
-		foreach ($tables as $table) {
-			$sql = "UPDATE $table SET updated=1, element_id = $id WHERE !updated && element_type = 'Product' && element_id IN (" . implode(', ', $ids) . ')';
-			// echo "$sql\n";
-			mysqli_query($mysqli, $sql) or die(mysqli_error($msyqli));
-		}
-	}
+foreach ($tables as $table) {
+	mysqli_query($mysqli, "UPDATE $table SET creator_id = user_id");
 }
