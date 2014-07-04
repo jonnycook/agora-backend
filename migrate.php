f3aac75f97cc37071922d560ccb06a7335855293
Jonny <qubsoft@gmail.com>
2014-07-04 13:59:31 +0700

    .

--- migrate.php
+++ migrate.php
@@ -1,23 +1,36 @@
 <?php
 
 require_once('includes/header.php');
-echo 'test';
-var_dump(ENV);
-$result = mysqli_query($mysqli, "SELECT id FROM m_users");
-while ($row = mysqli_fetch_assoc($result)) {
-	var_dump($row);
-	mysqli_query($mysqli, "INSERT INTO m_belts SET user_id = $row[id], creator_id = $row[id]");
-	$beltId = mysqli_insert_id($mysqli);
 
-	$rootElementsResult = mysqli_query($mysqli, "SELECT * FROM m_root_elements WHERE user_id = $row[id]");
-	while ($rootElement = mysqli_fetch_assoc($rootElementsResult)) {
-		mysqli_query($mysqli, "INSERT INTO m_belt_elements SET 
-			user_id = $rootElement[user_id],
-			belt_id = $beltId,
-			creator_id = $rootElement[creator_id],
-			element_type = '$rootElement[element_type]',
-			element_id = $rootElement[element_id],
-			`index` = $rootElement[index],
-			`created_at` = '$rootElement[created_at]'");
-	}
+// $result = mysqli_query($mysqli, "SELECT id FROM m_users");
+// while ($row = mysqli_fetch_assoc($result)) {
+// 	mysqli_query($mysqli, "INSERT INTO m_belts SET user_id = $row[id], creator_id = $row[id]");
+// 	$beltId = mysqli_insert_id($mysqli);
+
+// 	$rootElementsResult = mysqli_query($mysqli, "SELECT * FROM m_root_elements WHERE user_id = $row[id]");
+// 	while ($rootElement = mysqli_fetch_assoc($rootElementsResult)) {
+// 		mysqli_query($mysqli, "INSERT INTO m_belt_elements SET 
+// 			user_id = $rootElement[user_id],
+// 			belt_id = $beltId,
+// 			creator_id = $rootElement[creator_id],
+// 			element_type = '$rootElement[element_type]',
+// 			element_id = $rootElement[element_id],
+// 			`index` = $rootElement[index],
+// 			`created_at` = '$rootElement[created_at]'");
+// 	}
+// }
+
+$tables = array(
+	'm_list_elements',
+	'm_belt_elements',
+	'm_session_elements',
+	'm_bundle_elements',
+	'm_feelings',
+	'm_data',
+);
+
+$result = mysqli_query('SELECT * FROM m_products_old');
+while ($row = mysqli_fetch_assoc($result)) {
+	$products[$row['site_id']][$row['product_sid']][] = $row;
 }
+var_dump($products);
