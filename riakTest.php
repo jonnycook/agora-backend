7cd6536e21e37898244e4bc6bf8fac9a7154c35b
Jonny <qubsoft@gmail.com>
2014-07-07 20:51:32 +0700

    .

--- riakTest.php
+++ riakTest.php
@@ -15,7 +15,7 @@ require_once('includes/Basho/Riak/MapReduce/Phase.php');
 
 $riak = new Basho\Riak\Riak('50.116.31.117', 10018);
 
-$bucket->newObject($id, $this->riakData())->store();
+// $bucket->newObject($id, $this->riakData())->store();
 $riak->bucket('test')->newObject('test', 'test')->store();
 
 $bucket = $riak->bucket('test');
