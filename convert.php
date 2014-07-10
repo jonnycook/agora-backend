fcf04e795f64c54df283a15cba4ed066e90ebc99
Jonny <qubsoft@gmail.com>
2014-07-10 10:35:48 +0700

    .

---
+++ convert.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once('header.php');
+$userId = userId();
+
+$user = mysqli_fetch_assoc(mysqli_query($mysqli, "SELECT click_id, converted FROM m_users WHERE id = $userId"));
+var_dump($user);
+if (!$user['converted']) {
+	// $ch = curl_init("http://api.socialingot.com/postback/agora/?sid=$user[click_id]");
+	// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	// curl_exec($ch);
+	mysqli_query($mysqli, "UPDATE m_users SET converted = 1 WHERE id = $userId");
+}
