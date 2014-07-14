3b9b8bd5680c17e8b90056df274c522cc33629de
Jonny <qubsoft@gmail.com>
2014-07-14 18:00:10 +0700

    .

--- ext/ws/track.php
+++ ext/ws/track.php
@@ -6,7 +6,7 @@ $userId = getUserId($_GET['clientId']);
 
 $args = $_POST['args'];
 
-if ($args[0] == 'track') {
+if ($args[0] == 'visit') {
 	$siteId = Site::siteForName($args[1])->id;
 	mysqli_query($mysqli, "INSERT INTO visited SET user_id = $userId, site_id = $siteId");
 }
