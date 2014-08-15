4f9bddf6c965c5b1827820b8f5f31349bd6cb27e
Jonny <qubsoft@gmail.com>
2014-08-15 19:31:04 +0700

    .

--- signup.php
+++ signup.php
@@ -10,6 +10,7 @@ if ($_POST['submit']) {
 		$password = passHash($_POST['password']);
 		mysqli_query($mysqli, "INSERT INTO m_users SET email = '$email', name = '$name', password = '$password', created_at = UTC_TIMESTAMP()") or die(mysqli_error($mysqli));
 		$id = mysqli_insert_id($mysqli);
+		mysqli_query($mysqli, "INSERT INTO m_belts SET user_id = $id, creator_id = $id");
 		setUserId($id);
 		echo 'Successfully created account.';
 		exit;
