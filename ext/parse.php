4a817a60de4f6792099512094ba6342cfcd31296
Jonny <qubsoft@gmail.com>
2014-09-30 19:40:33 +0900

    .

--- ext/parse.php
+++ ext/parse.php
@@ -3,6 +3,9 @@ require_once('includes/header.php');
 
 require_once('../includes/parse.php');
 
+header('Access-Control-Allow-Origin: http://webapp.agora.dev');
+header('Access-Control-Allow-Origin: http://agora.sh');
+header('Access-Control-Allow-Credentials: true');
 header('Content-Type: application/json');
 
 echo json_encode(parse($_GET['descriptor']));
