48570d11e2cf23c2808cb53a77db4664f46d5c77
Jonny <qubsoft@gmail.com>
2014-10-10 10:48:04 +0900

    .

--- webapp/createUser.php
+++ webapp/createUser.php
@@ -6,7 +6,7 @@ require_once('../includes/user.php');
 $name = mysqli_real_escape_string($mysqli, $_POST['name']);
 $email = mysqli_real_escape_string($mysqli, $_POST['email']);
 
-mysqli_query($mysqli, "INSERT INTO m_users SET email = '$email', alerts_email = '$email', name = '$name', created_at = UTC_TIMESTAMP()");
+mysqli_query($mysqli, "INSERT INTO m_users SET email = '$email', alerts_email = '$email', name = '$name', created_at = UTC_TIMESTAMP(), `from` = 1");
 $id = mysqli_insert_id($mysqli);
 mysqli_query($mysqli, "INSERT INTO m_belts SET user_id = $id, creator_id = $id");
 
